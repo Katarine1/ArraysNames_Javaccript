61de312f4faf9758cb3ddf4f1a1392891b0e3028
Katarine1 <kba.2879@gmail.com>
2019-12-31 09:45:14 -0300

    Demonstration of Arrays forming a list of names

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Arrays
+
+let names = [];
+names[0] = "Paulo";
+names[1] = "Jose";
+names[2] = "Maria";
+names[3] = "Bruno";
+names[4] = "Paulina";
+names[5] = "Anna";
+
+console.log("List Names :");
+
+for(let i = 0 ; i < names.length ; i++)
+{	
+	console.log((i + 1) + ") " + names[i]);
+}
+
+
